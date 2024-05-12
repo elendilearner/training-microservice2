bad0c83fd004a6d90bd37f13f2f790ef56ca797b
elendilearner <elendilearner@gmail.com>
2024-05-12 07:03:30 +0300

    refactoring

--- main-application/src/main/java/com/education/trainingmicroservice/controller/BookingsController.java
+++ main-application/src/main/java/com/education/trainingmicroservice/controller/BookingsController.java
@@ -1,26 +1,25 @@
 package com.education.trainingmicroservice.controller;
 
 import com.education.api.V1Api;
-import com.education.api.model.AvailableSlotsResponseInner;
-import com.education.api.model.BookingRequest;
-import com.education.api.model.BookingResponse;
+import com.education.api.model.AvailableSlotsRs;
+import com.education.api.model.BookingRq;
+import com.education.api.model.BookingRs;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 public class BookingsController implements V1Api {
 
     @Override
-    public ResponseEntity<List<AvailableSlotsResponseInner>> v1ApiAvailableSlotsGet(LocalDate date) {
+    public ResponseEntity<AvailableSlotsRs> v1ApiAvailableSlotsGet(LocalDate date) {
         return V1Api.super.v1ApiAvailableSlotsGet(date);
     }
 
     @Override
-    public ResponseEntity<BookingResponse> v1ApiBookingsPost(BookingRequest bookingRequest) {
-        return ResponseEntity.ok(BookingResponse.builder()
+    public ResponseEntity<BookingRs> v1ApiBookingsPost(BookingRq bookingRequest) {
+        return ResponseEntity.ok(BookingRs.builder()
                 .bookingId("123")
                 .build());
     }
